f36b031de80dded29459d39f37bed4256a80aaa6
Hassan Pezeshk <hpez58@gmail.com>
2024-06-29 13:03:25 +0330

    feat: add support for url_id

---
+++ migrations/2024_06_29_122213_alter_gateway_transactions_add_url_id.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Hpez\Gateway\Enum;
+
+class CreateGatewayTransactionsTable extends Migration
+{
+	function getTable()
+	{
+		return config('gateway.table', 'gateway_transactions');
+	}
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table($this->getTable(), function (Blueprint $table) {
+			$table->string('url_id', 250)->nullable()->after('tracking_code');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table($this->getTable(), function (Blueprint $table) {
+            $table->dropColumn('url_id');
+        });
+	}
+}
